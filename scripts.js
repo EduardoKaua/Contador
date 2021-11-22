0ca29ef6bd242b548814c9be562a16ffde3daad3
Eduardo Kauã de Souza <edubr038@gmail.com>
2021-11-22 14:36:32 -0300

    Realizando java

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-const TT = document.getElementById = "Number"
+const TT = document.getElementById("number");
+
+let num = 0;
+
+function mais() {
+    TT.innerText = ++num;
+}
+function tirar() {
+    TT.innerText = --num;
+}
+function resetar() {
+    TT.innerText = num = 0;
+}
