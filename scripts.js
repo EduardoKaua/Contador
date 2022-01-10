1eeafa22a079c9121ca7d9aa7219d7008610be1e
Eduardo Kauã de Souza <edubr038@gmail.com>
2022-01-10 15:39:53 -0300

    .

--- scripts.js
+++ scripts.js
@@ -3,11 +3,11 @@ const TT = document.getElementById("number");
 let num = 0;
 
 function mais() {
-    TT.innerText = ++num;
+  TT.innerText = ++num;
 }
 function tirar() {
-    TT.innerText = --num;
+  TT.innerText = --num;
 }
 function resetar() {
-    TT.innerText = num = 0;
+  TT.innerText = num = 0;
 }
